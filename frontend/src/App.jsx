017a9a1d1be92b7c94ec2c2109ae1b19e9fd755f
prithishsamantae <prithishsamanta@gmail.com>
2025-06-29 00:23:16 -0400

    12-7-create-a-basic-event-page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Home from './pages/Home';
+import Vault from './pages/Vault';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/vault/:vaultId" element={<Vault />} />
       </Routes>
     </Router>
   );
